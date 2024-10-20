42b8095dd77a0dc920be5a45d95aee6b12a25324
MichalPolak01 <michalpl618@gmail.com>
2024-10-20 23:05:02 +0200

    Implement login tests

--- src/authentication/tests.py
+++ src/authentication/tests.py
@@ -1,3 +1,69 @@
 from django.test import TestCase
+from ninja_extra.testing import TestClient
+import pytest
+from .api import router
+from .models import User
 
-# Create your tests here.
+
+class NinjaAuthenticationTestCase(TestCase):
+    def setUp(self):
+        self.client = TestClient(router)
+
+        self.user = User.objects.create_user(username='JohnDoe123', email='johndoe@gmail.com', password='JohnDoe@!3')
+
+
+    @pytest.mark.django_db
+    def test_login_success(self):
+        """ Test login with correct user credentials """
+
+        # Arrange
+        payload = {
+            'email': 'johndoe@gmail.com',
+            'password': 'JohnDoe@!3'
+        }
+
+        # Act
+        response = self.client.post('/login', json=payload)
+
+        # Assert
+        assert response.status_code == 200
+        assert 'access' in response.json()
+        assert 'refresh' in response.json()
+
+
+    @pytest.mark.django_db
+    def test_login_invalid_emial(self):
+        """ Test login with invalid email """
+
+        # Arrange
+        payload = {
+            'email': 'johndoe@gmail.pl',
+            'password': 'JohnDoe@!3'
+        }
+
+        # Act
+        response = self.client.post('/login', json=payload)
+
+        # Assert
+        assert response.status_code == 401
+        assert response.json()['message'] == 'Invalid email or password'
+
+
+    @pytest.mark.django_db
+    def test_login_invalid_password(self):
+        """ Test login with invalid password """
+
+        # Arrange
+        payload = {
+            'email': 'johndoe@gmail.com',
+            'password': 'JohnDoe#$2'
+        }
+
+        # Act
+        response = self.client.post('/login', json=payload)
+
+        # Assert
+        assert response.status_code == 401
+        assert response.json()['message'] == 'Invalid email or password'
+
+
--- src/learn_how_to_code/api.py
+++ src/learn_how_to_code/api.py
@@ -1,5 +1,5 @@
 from ninja_extra import NinjaExtraAPI
 
-api = NinjaExtraAPI()
+api = NinjaExtraAPI(urls_namespace='myapi')
 
 api.add_router("", "authentication.api.router", tags=["Authentication"])
