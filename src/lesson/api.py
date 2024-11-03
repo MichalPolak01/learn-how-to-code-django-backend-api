30ae83fa1ae13adb8957540d2c2cf275d483d2cc
MichalPolak01 <michalpl618@gmail.com>
2024-11-03 18:03:02 +0100

    Create get lesson details endpoint

--- src/lesson/api.py
+++ src/lesson/api.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 
 router = Router()
 
-@router.post("/{module_id}/lessons", response={201: LessonDetailSchema, 404: MessageSchema, 500: MessageSchema}, auth=helpers.auth_required)
+@router.post("/modules/{module_id}/lessons", response={201: LessonDetailSchema, 404: MessageSchema, 500: MessageSchema}, auth=helpers.auth_required)
 def create_lesson(request, payload: LessonCreateSchema, module_id: int):
     """Creates a new lesson within a specific module."""
 
@@ -33,8 +33,8 @@ def create_lesson(request, payload: LessonCreateSchema, module_id: int):
         return 500, {"message": "An unexpected error occurred during lesson creation."}
     
 
-@router.get("/{module_id}/lessons", response={200: list[LessonDetailSchema], 404: MessageSchema, 500: MessageSchema}, auth=helpers.auth_required)
-def get_list_modules_for_course(request, module_id: int):
+@router.get("/modules/{module_id}/lessons", response={200: list[LessonDetailSchema], 404: MessageSchema, 500: MessageSchema}, auth=helpers.auth_required)
+def get_list_lessons_for_module(request, module_id: int):
     """Retrieves all lessons for a specific module."""
 
     try:
@@ -46,3 +46,17 @@ def get_list_modules_for_course(request, module_id: int):
         return 404, {"message": f"Module with id {module_id} not found."}
     except Exception as e:
         return 500, {"message": "An unexpected error occurred while retrieving list of lessons."}
+    
+
+@router.get("lessons/{lesson_id}", response={200: LessonDetailSchema, 404: MessageSchema, 500: MessageSchema}, auth=helpers.auth_required)
+def get_list_modules_for_course(request, lesson_id: int):
+    """Retrieves details of a specific lesson."""
+
+    try:
+        lesson = Lesson.objects.get(id=lesson_id)
+
+        return 200, lesson.to_dict()
+    except Lesson.DoesNotExist:
+        return 404, {"message": f"Lesson with id {lesson_id} not found."}
+    except Exception as e:
+        return 500, {"message": "An unexpected error occurred while retrieving the lesson."}
