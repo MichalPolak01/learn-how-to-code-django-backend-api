0d31b1f40414cfa3012ac5d7aa5a46a0ab9cef55
MichalPolak01 <michalpl618@gmail.com>
2024-11-03 14:46:10 +0100

    Create get list of modules for course endpoint

--- src/course/admin.py
+++ src/course/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Course, Rating
 
-admin.site.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    model = Course
+    list_display = ("id", "name", "author", "last_updated", "is_public")
+
+
+admin.site.register(Course, CourseAdmin)
--- src/module/api.py
+++ src/module/api.py
@@ -1,4 +1,4 @@
-from ninja import Router
+from ninja_extra import Router
 
 from .schemas import ModuleCreateSchema, ModuleUpdateSchema, ModuleDetailSchema
 from learn_how_to_code.schemas import MessageSchema
@@ -10,7 +10,6 @@ import helpers
 
 router = Router()
 
-
 @router.post("/{course_id}/modules", response={201: ModuleDetailSchema, 404: MessageSchema, 500: MessageSchema}, auth=helpers.auth_required)
 def create_course(request, payload: ModuleCreateSchema, course_id: int):
     """Creates a new module within a specific course."""
@@ -21,9 +20,25 @@ def create_course(request, payload: ModuleCreateSchema, course_id: int):
         module_data = payload.dict()
         module_data['course'] = course
         module_data['order'] = Module.get_next_order(course_id)
+
         module = Module.objects.create(**module_data)
 
         return 201, module.to_dict()
+    except Course.DoesNotExist:
+        return 404, {"message": f"Course with id {course_id} not found for the current user."}
+    except Exception as e:
+        return 500, {"message": "An unexpected error occurred during course creation."}
+    
+
+@router.get('/{course_id}/modules', response={200: list[ModuleDetailSchema], 404: MessageSchema, 500: MessageSchema}, auth=helpers.auth_required)
+def get_list_modules_for_course(request, course_id: int):
+    """Retrieves all modules for a specific course."""
+
+    try:
+        course = Course.objects.get(id=course_id)
+        modules = course.modules.all()
+
+        return 200, [ModuleDetailSchema(**module.to_dict()) for module in modules]
     except Course.DoesNotExist:
         return 404, {"message": f"Course with id {course_id} not found for the current user."}
     except Exception as e:
